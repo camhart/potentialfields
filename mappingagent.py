01248df7246cf6ef3fe980c04ebf4cadc13a9cae
James David Lambert <jamesdl@pineapple.cs.byu.edu>
2014-10-27 15:32:29 -0600

    Made target selection more intelligent

--- mappingagent.py
+++ mappingagent.py
@@ -82,31 +82,40 @@ class ObstacleData(object):
 
 	def isCharted(self, x, y):
 		return self.mapped[int(y) + self.width / 2, int(x) + self.height / 2] == 1
-		
-	def getUnchartedPoint(self, startX, startY):
 
+	def randomUnchartedPoint(self):
 		gridStepX = self.width / self.gridSize
 		gridStepY = self.height / self.gridSize
 
-		gridOffsetX = int(startX - self.gridSize / 2) / self.gridSize
-		gridOffsetY = int(startY - self.gridSize / 2) / self.gridSize
+		for tryCount in xrange(10):
+			x = random.randint(0, self.width - 1) - self.width / 2
+			y = random.randint(0, self.height - 1) - self.height / 2
 
-		#rough check
-		for y in xrange(gridStepY):
-			for x in xrange(gridStepX):
-				xPoint = self.gridSize * ((x + gridOffsetX) % gridStepX)
-				yPoint = self.gridSize * ((y + gridOffsetY) % gridStepY)
+			if not self.isCharted(x, y):
+				return x, y
 
-				if self.mapped[yPoint, xPoint] != 1:
-					return xPoint - self.width / 2, yPoint - self.height / 2
+		return None, None
+		
+		
+	def getUnchartedPoint(self, startX, startY):
 
-		#per pixel check
-		for y in xrange(self.height):
-			for x in xrange(self.width):
-				if self.mapped[x, y] != 1:
-					return x - self.width / 2, y - self.height / 2
+		result = None
+		distance = 0
 
-		return None
+		for sample in xrange(15):
+			pointX, pointY = self.randomUnchartedPoint()
+
+			if pointX != None:
+				currentDistance = (startX - pointX) ** 2 + (startY - pointY) ** 2
+
+				if result == None or currentDistance < distance:
+					result = (pointX, pointY)
+					distance = currentDistance
+				
+		if result != None:
+			return result
+		else:
+			return None
 
 def DrawObstacleData(data):
 	# This assumes you are using a numpy array for your grid
@@ -145,9 +154,6 @@ class FieldFollowTank(object):
 			# self.bzrTank.setSpeed(0)
 			self.bzrTank.rotateTowards(fieldDirUnit)
 
-		if self.bzrTank.shotsAvailable > 0:
-			self.bzrTank.shoot()
-
 class GridMappingTank(FieldFollowTank):
 	def __init__(self, bzrTank, game, color, obstacleData):
 		self.field = FieldManager()
@@ -160,6 +166,8 @@ class GridMappingTank(FieldFollowTank):
 		self.game = game
 		self.targetColor = color
 		self.obstacleData = obstacleData
+		self.lastPosition = complex(0, 0)
+		self.speed = 50.0
 
 
 	def update(self):
@@ -167,13 +175,25 @@ class GridMappingTank(FieldFollowTank):
 
 		if gridSample != None:
 			self.obstacleData.setOccSample(gridSample)
+		
+		currentSpeed = abs(self.lastPosition - self.bzrTank.position)
+		self.speed = self.speed * 0.7 + currentSpeed * 0.3
+		self.lastPosition = self.bzrTank.position
+		
+		isStuck = False
+		
+		if self.speed < 5:
+		  print("I'm stuck!")
+		  isStuck = True
 
-		if self.targetPoint != None and self.obstacleData.isCharted(self.targetPoint[0], self.targetPoint[1]):
+		if self.targetPoint == None or isStuck or self.obstacleData.isCharted(self.targetPoint[0], self.targetPoint[1]):
 			self.targetPoint = self.obstacleData.getUnchartedPoint(self.bzrTank.position.real, self.bzrTank.position.imag)
 
 			if self.targetPoint != None:
 				self.goalField.x = self.targetPoint[0]
 				self.goalField.y = self.targetPoint[1]
+				
+			self.speed = 50.0
 
 		super(GridMappingTank,self).update()
 
